d4010ae42cb1029994653862d95e851eecfde1e8
SalvatoreCavallaro1 <135614562+SalvatoreCavallaro1@users.noreply.github.com>
2024-02-18 12:21:16 +0100

    fix

--- lingo-mingle/components/videocall/index.js
+++ lingo-mingle/components/videocall/index.js
@@ -3,6 +3,6 @@ export { default as CustomBottomSheet } from "./CustomBottomSheet";
 export { default as CustomCallControl } from "./CustomCallControl";
 export { default as CustomCallControls } from "./CustomCallControls";
 export { default as CustomInput } from "./CustomInput";
-export { default as IncomingCall } from "./IncomingCall";
-export { default as OutgoingCall } from "./OutgoingCall";
+//export { default as IncomingCall } from "./IncomingCall";
+//export { default as OutgoingCall } from "./OutgoingCall";
 export {default as CustomCallTopView} from "./CustomCallTopView";
